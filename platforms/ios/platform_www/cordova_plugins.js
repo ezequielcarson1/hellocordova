c81c9c9528367669df0ea421cb088aea9ca0fc6e
Ezequiel Cason <ezequiel@Ezequiels-iMac.local>
2020-01-21 20:54:01 -0500

    First commit

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-dialogs.notification",
+      "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+      "pluginId": "cordova-plugin-dialogs",
+      "merges": [
+        "navigator.notification"
+      ]
+    },
+    {
+      "id": "cordova-plugin-statusbar.statusbar",
+      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+      "pluginId": "cordova-plugin-statusbar",
+      "clobbers": [
+        "window.StatusBar"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-dialogs": "2.0.2",
+    "cordova-plugin-statusbar": "2.4.3"
+  };
+});
